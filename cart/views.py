d9e2550b4ded41b8868747d7f5e92a65c7daf46a
Doru Muresanu <doru.muresanu@yahoo.ie>
2025-03-21 23:49:47 +0000

    Added cart adjustment/removal views and subtotal filter

--- cart/views.py
+++ cart/views.py
@@ -1,24 +1,45 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_cart(request):
     """ A view that renders the cart contents page """
-
     return render(request, 'cart/cart.html')
 
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
+    if item_id in cart:
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust the quantity of the specified product to the given amount """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id, None)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Remove an item from the shopping cart """
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id, None)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
