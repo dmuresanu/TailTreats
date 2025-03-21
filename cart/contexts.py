40841bc1f86dc9cf8859ee5312d598e23e45658d
Doru Muresanu <doru.muresanu@yahoo.ie>
2025-03-21 22:31:43 +0000

    Added quantity boxes to shopping cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def cart_contents(request):
-
     cart_items = []
     total = 0
     product_count = 0
@@ -15,7 +14,7 @@ def cart_contents(request):
         total += quantity * product.price
         product_count += quantity
         cart_items.append({
-            'item_id':item_id,
+            'item_id': item_id,
             'quantity': quantity,
             'product': product,
         })
@@ -27,16 +26,16 @@ def cart_contents(request):
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total        
+    grand_total = delivery + total
 
     context = {
-        'cart_items' : cart_items,
-        'total' : total,
-        'product_count' : product_count,
-        'delivery' : delivery,
-        'free_delivery_delta' : free_delivery_delta,
-        'free_delivery_threshold' : settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total' : grand_total,
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
