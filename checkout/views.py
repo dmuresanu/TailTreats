943726aa17c1b8ec69037ef465d7d08a585be07e
Doru Muresanu <doru.muresanu@yahoo.ie>
2025-03-22 20:25:48 +0000

    Added Stripe Elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R5XksCpP0dbs1R9fBh7qccnm8yfWyUOjLy1zHVcVUWhDLpujSwc5J1I3AWaLicAh3WbhywhpUrYDyL6vgZ4QxAC00Lxs1N05n',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
