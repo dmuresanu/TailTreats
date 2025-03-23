661a6379de96004cab8e9c4f01ffd46375103bac
Doru Muresanu <doru.muresanu@yahoo.ie>
2025-03-23 15:52:12 +0000

    Add custom_storages for AWS S3

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
+    location = settings.STATICFILES_LOCATION
     default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.MEDIAFILES_LOCATION
     file_overwrite = False
+    default_acl = 'public-read'
